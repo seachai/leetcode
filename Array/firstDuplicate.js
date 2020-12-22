482b1dca4ebc21867b9fe6fe6a57f5f819391010
Chai Lee <imchai@gmail.com>
2020-12-22 09:04:29 -0500

    first dupe

---
+++ Array/firstDuplicate.js
@@ -0,0 +1,34 @@
+/*
+Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
+
+Example
+
+For a = [2, 1, 3, 5, 3, 2], the output should be firstDuplicate(a) = 3.
+
+There are 2 duplicates: numbers 2 and 3. The second occurrence of 3 has a smaller index than the second occurrence of 2 does, so the answer is 3.
+
+For a = [2, 2], the output should be firstDuplicate(a) = 2;
+
+For a = [2, 4, 3, 5, 1], the output should be firstDuplicate(a) = -1.
+*/
+
+// TC: O(n)
+// SC: O(n)
+
+function firstDuplicate(a) {
+  const uniqSet = new Set();
+
+  for (let i = 0; i < a.length; i++) {
+    const number = a[i];
+    // Add the curr number to the set if we don't have it
+    if (!uniqSet.has(number)) {
+      uniqSet.add(number);
+    } else {
+      // If we have the curr number return it, since it's a dupe
+      return number;
+    }
+  }
+
+  // Otherwise return -1 since no dupes are found
+  return -1;
+}
